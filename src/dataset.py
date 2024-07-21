a2ae2adc52996a5b73ee321781c2141391b25a14
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-07-21 14:57:02 +0530

    Changed loaders such that the dataset is split into train, validation and test sets

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,3 @@
-# dataset.py
 import os
 import pandas as pd
 import numpy as np
@@ -10,17 +9,37 @@ from torchvision import transforms
 from itertools import chain
 import config
 
+def load_image_lists():
+    with open(os.path.join(config.BASE_DATA_PATH, 'train_val_list.txt'), 'r') as file:
+        train_val_images = file.read().splitlines()
+    with open(os.path.join(config.BASE_DATA_PATH, 'test_list.txt'), 'r') as file:
+        test_images = file.read().splitlines()
+        
+    return train_val_images, test_images
+
 def load_data():
+    train_val_images, test_images = load_image_lists()
+    
     data = pd.read_csv(config.CSV_FILE)
     data = data[data['Patient Age'] < 100]
+    data['image_file'] = data['Image Index'].apply(lambda x: x.split('.')[0] + '.png')  # Ensure format matches the lists
     image_paths = glob(config.IMAGE_PATH_PATTERN)
     image_path_dict = {os.path.basename(path): path for path in image_paths}
-    data['path'] = data['Image Index'].map(image_path_dict.get)
-    data[config.FINDING_LABELS_COLUMN] = data[config.FINDING_LABELS_COLUMN].apply(lambda x: x.replace('No Finding', ''))
-    all_labels = sorted(np.unique(list(chain(*data[config.FINDING_LABELS_COLUMN].map(lambda x: x.split('|')).tolist()))))
+    data['path'] = data['image_file'].map(image_path_dict.get)
+
+    # Apply filters based on the lists
+    train_val_data = data[data['image_file'].isin(train_val_images)]
+    test_data = data[data['image_file'].isin(test_images)]
+    
+    train_val_data[config.FINDING_LABELS_COLUMN] = train_val_data[config.FINDING_LABELS_COLUMN].apply(lambda x: x.replace('No Finding', ''))
+    test_data[config.FINDING_LABELS_COLUMN] = test_data[config.FINDING_LABELS_COLUMN].apply(lambda x: x.replace('No Finding', ''))
+    
+    all_labels = sorted(np.unique(list(chain(*train_val_data[config.FINDING_LABELS_COLUMN].map(lambda x: x.split('|')).tolist()))))
     for label in all_labels:
-        data[label] = data[config.FINDING_LABELS_COLUMN].apply(lambda x, lbl=label: 1.0 if lbl in x else 0)
-    return data, all_labels
+        train_val_data[label] = train_val_data[config.FINDING_LABELS_COLUMN].apply(lambda x, lbl=label: 1.0 if lbl in x else 0)
+        test_data[label] = test_data[config.FINDING_LABELS_COLUMN].apply(lambda x, lbl=label: 1.0 if lbl in x else 0)
+
+    return train_val_data, test_data, all_labels
 
 def get_transforms():
     return transforms.Compose([
@@ -47,6 +66,7 @@ class ChestXrayDataset(Dataset):
         label = self.dataframe.iloc[idx][self.labels].values.astype(np.float32)
         if self.transform:
             image = self.transform(image)
+
         return image, label
 
 def prepare_data_loaders(data, all_labels, batch_size=config.BATCH_SIZE, val_batch_size=config.VAL_BATCH_SIZE):
@@ -56,5 +76,11 @@ def prepare_data_loaders(data, all_labels, batch_size=config.BATCH_SIZE, val_bat
     train_dataset, valid_dataset = torch.utils.data.random_split(dataset, [train_size, valid_size])
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     valid_loader = DataLoader(valid_dataset, batch_size=val_batch_size, shuffle=False)
-
+    
     return train_loader, valid_loader
+
+def prepare_test_loader(test_data, all_labels, batch_size=config.VAL_BATCH_SIZE):
+    test_dataset = ChestXrayDataset(test_data, all_labels, transform=get_transforms())
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return test_loader
--- src/main.py
+++ src/main.py
@@ -2,20 +2,20 @@ import torch
 
 from utils import initialize_model, save_model
 from train import train_model
-from dataset import load_data, prepare_data_loaders
+from dataset import load_data, prepare_data_loaders, prepare_test_loader
 from visualize import plot_loss, plot_accuracy, plot_f1_precision_recall, plot_auc
 import config
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    data, all_labels = load_data()
-    train_loader, valid_loader = prepare_data_loaders(data, all_labels)
+    train_val_data, test_data, all_labels = load_data()
+    
+    train_loader, valid_loader = prepare_data_loaders(train_val_data, all_labels)
+    test_loader = prepare_test_loader(test_data, all_labels)
     
     model, criterion, optimizer, scheduler = initialize_model(device, num_classes=len(all_labels))
-
-    # Set save_model to True if you want to save the model after training
     trained_model, best_trained_model, metrics_history = train_model(model, criterion, optimizer, scheduler, train_loader, valid_loader, device, num_epochs=config.NUM_EPOCHS, save_model=True, save_path=config.MODEL_SAVE_PATH)
-
+    
     # Save the model with the best validation accuracy
     save_model(best_trained_model, 'model_weights/final_best_model.pth')
     save_model(trained_model, 'model_weights/complete_trained_model.pth')
